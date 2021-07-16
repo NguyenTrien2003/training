7aecfc34e15bf4d891dac88986e14169b0ddf8d7
NguyenTrien2003 <trientrien15@gmail.com>
2021-07-16 11:02:08 +0700

    Add index.html changed

--- main.js
+++ main.js
@@ -1,60 +1,24 @@
-/*
-alert('Hi i am Trien')
-var fullName = 'Nguyen Tien Trien'
-var age = 18
-alert(fullName)
-alert(age)
-console.log('Anh yeu em')
-console.log(fullName)
-confirm('xac nhan tren 18 tuoi!')
-prompt('ban bao nhieu tuoi?')
-setTimeout(function() {
-    alert('you dont 18')
-}, 1000)
-setInterval(function() {
-    console.log('dung khoc nhe')
-}, 1000)
 
-// toan tu so hoc
-var a = 1 + 2
-console.log(a)
-var b = 3
-var c = 4
-if (b < c) {
-    alert('Dung');
+function addition(a, b){
+    return a + b 
 }
-var v = 1
-var n = 2
-if (v > 0 && n > 0) {
-    alert('v & n lon lon 0')
-}
-setInterval(function() {
-    console.log('dont kill me' + Math.random(555))
-}, 1000)
-var firstName = 'Nguyen'
-var lastName = 'Trien'
-console.log(firstName + ' ' + lastName)
-var h = 1  
-var j = 2
-if (h < j) {
-    console.log('Dieu kien dung!')
-} else {
-    console.log('Dieu kien sai!')
-}
-var k = 1   
-var l = 2
-var isSuccess = k < l
-console.log(isSuccess)
+var result = addition(3, 2)
 
-var clean = 1 < 2
-if (clean) {
-    console.log('Dieu kien dung!')
-} else {
-    console.log('Dieu kien sai!')
-}
-*/
+console.log(result)
 
-var a = 1
-var b = 2
-function tinh_tong(a,b)
-return a + b
+
+
+function addition(a, b){
+    return a + b
+}
+var result = addition(-3, -6)
+console.log(result)
+
+
+
+function addition(a, b){
+    return a + b
+}
+var result = addition(7, 3)
+console.log(result)
+
