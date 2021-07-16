7c4f3d80e84c7b33fbee845108dcef2f91c28813
NguyenTrien2003 <trientrien15@gmail.com>
2021-07-16 09:29:51 +0700

    Add new file index.html

---
+++ main.js
@@ -0,0 +1,60 @@
+/*
+alert('Hi i am Trien')
+var fullName = 'Nguyen Tien Trien'
+var age = 18
+alert(fullName)
+alert(age)
+console.log('Anh yeu em')
+console.log(fullName)
+confirm('xac nhan tren 18 tuoi!')
+prompt('ban bao nhieu tuoi?')
+setTimeout(function() {
+    alert('you dont 18')
+}, 1000)
+setInterval(function() {
+    console.log('dung khoc nhe')
+}, 1000)
+
+// toan tu so hoc
+var a = 1 + 2
+console.log(a)
+var b = 3
+var c = 4
+if (b < c) {
+    alert('Dung');
+}
+var v = 1
+var n = 2
+if (v > 0 && n > 0) {
+    alert('v & n lon lon 0')
+}
+setInterval(function() {
+    console.log('dont kill me' + Math.random(555))
+}, 1000)
+var firstName = 'Nguyen'
+var lastName = 'Trien'
+console.log(firstName + ' ' + lastName)
+var h = 1  
+var j = 2
+if (h < j) {
+    console.log('Dieu kien dung!')
+} else {
+    console.log('Dieu kien sai!')
+}
+var k = 1   
+var l = 2
+var isSuccess = k < l
+console.log(isSuccess)
+
+var clean = 1 < 2
+if (clean) {
+    console.log('Dieu kien dung!')
+} else {
+    console.log('Dieu kien sai!')
+}
+*/
+
+var a = 1
+var b = 2
+function tinh_tong(a,b)
+return a + b
